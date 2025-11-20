d7a2b2a730bfd01f778384cafafef43856a6dbb0
TwiistedHypers <twiistedhypers@gmail.com>
2025-11-20 14:43:11 +0700

    modify url

--- resources/valleyofNamada.sh
+++ resources/valleyofNamada.sh
@@ -123,7 +123,7 @@ function deploy_validator_node() {
     echo -e "- This script ${GREEN}DOES NOT${RESET} send any data outside your server"
     echo "- All operations are performed locally"
     echo "- You are encouraged to audit the script at:"
-    echo -e "  ${BLUE}https://github.com/hubofvalley/Mainnet-Guides/blob/main/Namada/resources/namada_validator_node_install_mainnet.sh${RESET}"
+    echo -e "  ${BLUE}https://github.com/hubofvalley/Valley-of-Namada-Mainnet/blob/main//resources/namada_validator_node_install_mainnet.sh${RESET}"
 
     echo -e "\n${YELLOW}2. SYSTEM IMPACT:${RESET}"
     echo -e "${GREEN}New Services:${RESET}"
