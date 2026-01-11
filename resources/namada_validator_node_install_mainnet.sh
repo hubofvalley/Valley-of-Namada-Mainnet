f62e8167c552681c006effdaa8a8471b391dd73e
Grand Valley <letsbuidltogether@grandvalleys.com>
2026-01-11 17:06:20 +0700

    Update CometBft version and installation method

--- resources/namada_validator_node_install_mainnet.sh
+++ resources/namada_validator_node_install_mainnet.sh
@@ -67,13 +67,23 @@ rustc --version
 
 cd $HOME
 rm -rf cometbft
-git clone https://github.com/cometbft/cometbft.git
+mkdir -p cometbft
 cd cometbft
-git fetch --all
-git checkout v0.37.16
-make build
-sudo cp $HOME/cometbft/build/cometbft /usr/local/bin/
+wget -O cometbft_v0.37.17.tar.gz https://github.com/heliaxdev/cometbft/releases/download/v0.37.17/cometbft_0.37.17_linux_amd64.tar.gz
+tar -xzf cometbft_v0.37.17.tar.gz
+sudo chmod +x cometbft
+sudo cp cometbft /usr/local/bin/
 cometbft version
+
+#cd $HOME
+#rm -rf cometbft
+#git clone https://github.com/cometbft/cometbft.git
+#cd cometbft
+#git fetch --all
+#git checkout v0.37.17
+#make build
+#sudo cp $HOME/cometbft/build/cometbft /usr/local/bin/
+#cometbft version
 
 # 5. Set environment variables
 echo 'export MONIKER="$MONIKER"' >> $HOME/.bash_profile
